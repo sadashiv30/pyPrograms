88cf3a159c0d34a0878784b477f96d4c4ca622b0
sadashiv30 <sadashiv.30@gmail.com>
2015-04-05 19:00:18 -0700

    exercise 3.3

--- exercise3_1.py
+++ exercise3_1.py
@@ -4,10 +4,10 @@ Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all
 Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). 
 You should use raw_input to read a string and float() to convert the string to a number.
 Do not worry about error checking the user input - assume the user types numbers properly.'''
-rate = (float) raw_input("Enter rate ")
-hours = (float)raw_input("Enter hours ")
-if(hours > 40)
-    pay = (rate)*(40)+ 1.5 * (rate)* (hours -40)
-else
+rate = float(raw_input("Enter rate "))
+hours = float(raw_input("Enter hours "))
+if(hours > 40):
+    pay = (rate) * (40) + 1.5 * (rate) * (hours - 40)
+else:
     pay = rate * hours
 print pay   
